c4268e1a911ddf83709280dd14f8f1895b920995
최한솔 <hsc080808@naver.com>
2022-02-19 14:26:10 +0900

    add comments

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import styled, { keyframes } from 'styled-components';
 2. as: HTML 태그 내에 선언. as 내의 값으로 tag가 결정
 3. attr(): 태그의 속성값을 정의
 4. 생성한 컴포넌트명을 통해서도 css 정의가 가능 ex) Emoji
-5. props을 통해서도 css 프로퍼티를 정해 줄 수 있음.
+5. props을 통해서도 css 프로퍼티를 정해 줄 수 있음. ex) state selector(css selector)
+6. animations: ex) keyframes`from {} to {}`
 */
 
 const Father = styled.div`
