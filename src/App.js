9f6f5a0d81cbee221e6c108b5391878874bf5dd2
최한솔 <hsc080808@naver.com>
2022-02-19 14:25:47 +0900

    ongoing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from 'styled-components';
+
+/* keywords
+1. styled(): 다른 컴포넌트의 속성 값을 재 사용해야 할 경우
+2. as: HTML 태그 내에 선언. as 내의 값으로 tag가 결정
+3. attr(): 태그의 속성값을 정의
+4. 생성한 컴포넌트명을 통해서도 css 정의가 가능 ex) Emoji
+5. props을 통해서도 css 프로퍼티를 정해 줄 수 있음.
+*/
+
+const Father = styled.div`
+  display: flex;
+`
+
+const rotateAnimation = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+const MySpan = styled.span`
+  font-size: 36px;
+`
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  animation: ${rotateAnimation} linear 1s infinite;
+  ${MySpan}{
+    &:hover{
+      font-size: 70px;
+    }
+  }
+`
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+     <Father>
+       <Box>
+         <MySpan>hello world</MySpan>
+       </Box>
+     </Father>
+   )
 }
 
 export default App;
